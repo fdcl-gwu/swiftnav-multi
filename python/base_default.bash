2b9c901678b364f30b485e32c4448a42a6bd0499
Kanishke-FDCL <kanishkegb@gwu.edu>
2018-04-11 15:57:18 -0400

    updated new lines in bash scripts

--- python/base_default.bash
+++ python/base_default.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "\nSetting base GPS to default settings ...\n"
+echo " "
+echo "Setting base GPS to default settings ..."
 python2 write_from_ini_file.py -f settings_files/base_default.ini
-echo "\nTask finished."
+echo " "
+echo "Task finished."
--- python/base_simulator.bash
+++ python/base_simulator.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "\nSetting base GPS to simulator mode ...\n"
+echo " "
+echo "Setting base GPS to simulator mode ..."
 python2 write_from_ini_file.py -f settings_files/base_simulator_mode.ini
-echo "\nTask finished."
+echo " "
+echo "Task finished."
